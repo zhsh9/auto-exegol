1bfcebe035c07fc97599435f0a78df784247ce03
Shuhao Zhang <eclipse.tenet@gmail.com>
2023-06-04 01:54:13 +0800

    Add variable SOCAT_PORT

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #@Author: zhsh9
 
+# SOCAT_PORT config.
+SOCAT_PORT=6000
+
 # Start xquartz.
 echo '[+] open -a XQuartz'
 open -a XQuartz
 echo -e "\033[32m[*] Success start XQuartz!\033[0m" # INFO
 
 # Start socat LISTEN.
-echo '[+] socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &'
-socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+echo -e "[+] socat TCP-LISTEN:$SOCAT_PORT,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &"
+socat TCP-LISTEN:$SOCAT_PORT,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 echo -e '\033[32m[*] Success start socat!\033[0m' # INFO
 
 # Start exegol
